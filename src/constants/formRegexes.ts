f813e21127c78f0f1b14b7ffd3e3f546c487e24e
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2021-02-27 23:56:20 +0300

    New password regex

--- src/constants/formRegexes.ts
+++ src/constants/formRegexes.ts
@@ -2,7 +2,8 @@
 const emailRegex = new RegExp(
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
-const passwordRegex = new RegExp(
-  "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
+const strongPasswordRegex = new RegExp(
+  "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})"
 );
-export { emailRegex, passwordRegex };
+const passwordRegex = new RegExp("^.{8,}");
+export { emailRegex, passwordRegex, strongPasswordRegex };
